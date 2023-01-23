e6309fabc383873ed0b2656095a4b7a387e38137
ssiriukaitis <sigitas.siriukaitis@gmail.com>
2023-01-23 14:03:30 +0200

    click ir longclick padaryti

--- app/src/main/java/lt/sigitas/mynotes/MainActivity.java
+++ app/src/main/java/lt/sigitas/mynotes/MainActivity.java
@@ -2,10 +2,16 @@ package lt.sigitas.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,6 +19,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "my_notes_main";
     private ListView listView;
     private ArrayAdapter<Note> adapter;
 
@@ -27,6 +34,25 @@ public class MainActivity extends AppCompatActivity {
         setUpListViewAdapter(notesList);
 
 
+        setUpClick(notesList);
+        setUpLongClick();
+
+
+    }
+
+    private List<Note> generateNotesList(int recordNo) {
+        List<Note> notesList = new ArrayList<>();
+        for (int i = 1; i <= recordNo; i++){
+            Note note = new Note(i, "name" + i, "some note text" + i);
+
+            notesList.add(note);
+        }
+        return notesList;
+    }
+
+    private void setUpViews() {
+        setContentView(R.layout.activity_main);
+        this.listView = findViewById(R.id.listView);
     }
 
     private void setUpListViewAdapter(List<Note> notesList) {
@@ -39,20 +65,30 @@ public class MainActivity extends AppCompatActivity {
         this.listView.setAdapter(adapter);
     }
 
-    private void setUpViews() {
-        setContentView(R.layout.activity_main);
-        this.listView = findViewById(R.id.listView);
+    private void setUpClick(List<Note> notesList) {
+        AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Log.i(TAG, "onItemClick" + position + ", #:" + notesList.get(position));
+
+            }
+        };
+        listView.setOnItemClickListener(listener);
+    }
+    private void setUpLongClick() {
+        AdapterView.OnItemLongClickListener listenerLongClick = new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                return false;
+            }
+
+        };
+        listView.setOnItemLongClickListener(listenerLongClick);
     }
 
 
-    private List<Note> generateNotesList(int recordNo) {
-        List<Note> notesList = new ArrayList<>();
-        for (int i = 1; i <= recordNo; i++){
-            Note note = new Note(i, "name" + i, "some note text" + i);
 
-            notesList.add(note);
-        }
-        return notesList;
-    }
+
+
 
 }
